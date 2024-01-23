61d4e0d4358755f8a838629e0e700085676bab19
Tahir Javed <tahirjavedbhutta@gmail.com>
2024-01-24 04:28:29 +0500

    Updated the keypair name

--- Terraform/key_pair.tf
+++ Terraform/key_pair.tf
@@ -4,15 +4,15 @@ resource "tls_private_key" "example" {
 }
 
 resource "aws_key_pair" "generated_key" {
-  key_name   = "test-${var.aws_region}"
+  key_name   = "keypair-${var.aws_region}"
   public_key = tls_private_key.example.public_key_openssh
 
   tags = {
-    Name = "test-${var.aws_region}"
+    Name = "keypair-${var.aws_region}"
   }
 }
 
 resource "local_file" "private_key" {
   content  = tls_private_key.example.private_key_pem
-  filename = "${path.module}/test-${var.aws_region}.pem"
+  filename = "${path.module}/keypair-${var.aws_region}.pem"
 }
